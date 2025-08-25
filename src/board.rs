c52dbd5bb11e3ec83f755548da982ed82472f154
lonely43 <iladenisov327@gmail.com>
2025-08-26 00:53:42 +0300

    feat: create board

---
+++ src/board.rs
@@ -0,0 +1,84 @@
+#[derive(Debug)]
+pub struct Board {
+    cells: [[i8; 8]; 8],
+}
+
+impl Board {
+    pub fn new() -> Self {
+        let mut cells = [[0; 8]; 8];
+
+        for row in 0..8 {
+            for col in 0..8 {
+                if (row + col) % 2 == 1 {
+                    if row < 3 {
+                        // black piece
+                        cells[row][col] = -1;
+                    } else if row > 4 {
+                        // white piece
+                        cells[row][col] = 1;
+                    }
+                }
+            }
+        }
+
+        Board { cells }
+    }
+
+    pub fn render(&self) {
+        println!("    a  b  c  d  e  f  g  h   ");
+        println!("  +------------------------+");
+
+        for (row, cells_in_col) in self.cells.iter().enumerate() {
+            print!("{} |", 8 - row);
+
+            for (col, cell) in cells_in_col.iter().enumerate() {
+                let symbol = match cell {
+                    1 => " ● ",
+                    -1 => " ○ ",
+                    _ => {
+                        if (row + col) % 2 == 0 {
+                            "░░░"
+                        } else {
+                            "   "
+                        }
+                    }
+                };
+                print!("{}", symbol);
+            }
+
+            println!("| {}", 8 - row);
+        }
+
+        println!("  +------------------------+");
+        println!("    a  b  c  d  e  f  g  h   ");
+    }
+
+    pub fn debug_render(&self) {
+        println!("    a  b  c  d  e  f  g  h   ");
+        println!("  +------------------------+");
+
+        for (row, cells_in_col) in self.cells.iter().enumerate() {
+            print!("{} |", 8 - row);
+
+            for (col, cell) in cells_in_col.iter().enumerate() {
+                let symbol = match cell {
+                    1 => " 1 ",
+                    -1 => "-1 ",
+                    _ => {
+                        if (row + col) % 2 == 0 {
+                            "░░░"
+                        } else {
+                            "   "
+                        }
+                    }
+                };
+                print!("{}", symbol);
+            }
+
+            println!("| {}", 8 - row);
+        }
+
+        println!("  +------------------------+");
+        println!("    a  b  c  d  e  f  g  h   ");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod board;
+
+use board::Board;
+
+#[allow(dead_code)]
 fn main() {
-    println!("Hello, world!");
+    let board: Board = Board::new();
+
+  //  board.render();
+
+    board.debug_render();
 }
