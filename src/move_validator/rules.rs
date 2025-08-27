00cd5420904c1ab48ebd21260989df50cf6acdfb
lonely43 <iladenisov327@gmail.com>
2025-08-27 14:54:33 +0300

    chore: planning

--- src/move_validator/rules.rs
+++ src/move_validator/rules.rs
@@ -1,6 +1,8 @@
 use crate::board::Board;
 #[allow(dead_code, unused)]
 pub fn check_basic_rules(board: &Board, from: (usize, usize), to: (usize, usize), player: i8) -> bool {
+    // check limits
+
     // is it piece
     if board.get_cell(from) == 0 {
         println!("{},{} - it is the empty cell", from.0, from.1);
@@ -12,6 +14,8 @@ pub fn check_basic_rules(board: &Board, from: (usize, usize), to: (usize, usize)
         println!("{},{} - choose your piece", from.0, from.1);
         return false;
     }
+
+    //check is it a diagonal move
 
     true
 }
