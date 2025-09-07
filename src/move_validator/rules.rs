6669beeb077142266d9c52566584796464f9528f
lonely43 <iladenisov327@gmail.com>
2025-09-07 15:40:41 +0300

    refactor: clean up code

--- src/move_validator/rules.rs
+++ src/move_validator/rules.rs
@@ -1,14 +1,14 @@
 use crate::board::Board;
 
 pub fn is_in_board(pos: (usize, usize)) -> bool {
-    if !(pos.0 < 8) {
-        println!("{} - uncorrect row", pos.0 + 1);
-        println!("type a correct position around 0-7: ");
-        return false;
-    }
     if !(pos.1 < 8) {
         println!("{} - uncorrect column", pos.1);
         println!("type a correct position around a-h: ");
+        return false;
+    }
+    if !(pos.0 < 8) {
+        println!("{} - uncorrect row", pos.0 + 1);
+        println!("type a correct position around 1-8: ");
         return false;
     }
 
@@ -17,27 +17,29 @@ pub fn is_in_board(pos: (usize, usize)) -> bool {
 
 #[allow(dead_code, unused)]
 pub fn check_basic_rules(board: &Board, from: (usize, usize), to: (usize, usize), player: i8) -> bool {
+    let valid_columns = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
     // 1. check limits
     if !is_in_board(from) || !is_in_board(to) {
-        println!("impossible positision");
+        println!("{}{} - impossible position", valid_columns[from.1], from.0 + 1);
         return false;
     }
 
     // 2. is the "from" a piece
     if board.get_cell(from) == 0 {
-        println!("{},{} - it is not a piece", from.0 + 1, from.1);
+        println!("{}{} - is not a piece", valid_columns[from.1], from.0 + 1);
         return false;
     }
 
     // 3. is it player's piece
     if !(board.get_cell(from) == player || board.get_cell(from) == player + 1 || board.get_cell(from) == player - 1) {
-        println!("{},{} - choose your piece", from.0, from.1);
+        println!("{}{} - you don't own that piece", valid_columns[from.1], from.0 + 1);
         return false;
     }
 
     // 4. is the target occupated by player already
     if board.get_cell(to) == player {
-        println!("That cell is already occupated by you");
+        println!("{}{} is already occupated by you", valid_columns[from.1], from.0 + 1);
         return false;
     }
 
